094fbd4af921ed80773548f4213993f53e325017
WilliamRCR <williamcastanazaromero2@gmail.com>
2026-05-21 15:35:13 -0600

    Configuracion de aplicacion

--- src/main/java/com/pisoplaza/controller/AsignacionEspacioController.java
+++ src/main/java/com/pisoplaza/controller/AsignacionEspacioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/asignacionespacios")
+@RequestMapping("/api/asignaciones")
 @RequiredArgsConstructor
 public class AsignacionEspacioController {
     private final AsignacionEspacioService service;
